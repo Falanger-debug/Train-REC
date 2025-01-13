1fbfa2e88186c73b7e27b9e751c33369e591cbb6
falan <falanger27@gmail.com>
2025-01-13 16:16:01 +0100

    summary works

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -4,27 +4,27 @@ const renderMainPage = (req, res) => {
 
 const renderTrainRoutes = (req, res) => {
     res.render('trainRoutes');
-}
+};
 
 const renderWallet = (req, res) => {
     res.render('wallet');
-}
+};
 
 const renderUserProfile = (req, res) => {
     res.render('userProfile');
-}
+};
 
 const renderLogin = (req, res) => {
     res.render('login');
-}
+};
 
 const renderRegister = (req, res) => {
     res.render('register');
-}
+};
 
 const renderModals = (req, res) => {
     res.render('modals');
-}
+};
 const searchConnections = (req, res) => {
     const { from, to, date, hour } = req.body;
     const connection = {
@@ -34,10 +34,15 @@ const searchConnections = (req, res) => {
         hour
     };
     res.render('trainRoutes', { connection });
-}
+};
 
 const renderBuyTicket = (req, res) => {
     const { from, to, duration, connections, price, wherefrom, whereto } = req.query;
+
+    if (!from || !to || !duration || !price) {
+        return res.status(400).send("Missing required query parameters.");
+    }
+
     const ticket = {
         from,
         to,
@@ -48,17 +53,21 @@ const renderBuyTicket = (req, res) => {
         whereto
     }
     res.render('buyTicket', {ticket});
-}
+};
 
 const renderSummary = (req, res) => {
-    const { class: selectedClass, discount, seat, price } = req.query;
+    const { class: wherefrom, whereto, from, to, selectedClass, discount, seat, finalPrice } = req.query;
     res.render('summary', {
+        wherefrom: wherefrom || 'Nie wybrano',
+        whereto: whereto || 'Nie wybrano',
+        from: from || 'Nie wybrano',
+        to: to || 'Nie wybrano',
         selectedClass: selectedClass || 'Nie wybrano',
         discount: discount || 'Brak ulgi',
         seat: seat || 'Nie wybrane',
-        price: price || 'Brak ceny'
+        finalPrice: finalPrice || 'Brak ceny'
     });
-}
+};
 
 export { renderMainPage,
     renderTrainRoutes,
