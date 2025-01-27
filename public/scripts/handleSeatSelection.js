1af0945fefaccd0c809a00ca1db6861437c5e3d1
Falanger-debug <falanger27@gmail.com>
2025-01-27 23:49:30 +0100

    final v for now

--- public/scripts/handleSeatSelection.js
+++ public/scripts/handleSeatSelection.js
@@ -1,23 +1,4 @@
-// document.addEventListener('DOMContentLoaded', () => {
-//     const schematOption = document.getElementById('schematOption');
-//
-//     schematOption.addEventListener('click', () => {
-//         if (schematOption.selected) {
-//             window.location.href = '/seatChoice';
-//         }
-//     });
-// });
-//
-//
-// function handleSeatSelection() {
-//     const seatSelect = document.getElementById('seat');
-//     const selectedValue = seatSelect.value;
-//
-//     if (selectedValue === 'schemat') {
-//         window.location.href = '/seatChoice';
-//     }
-//     // obsługa innych wartości
-// }
+
 function handleSeatSelection() {
 
     const selectElement = document.getElementById('seat');
